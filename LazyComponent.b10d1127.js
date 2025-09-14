26bb0a1fd716d6bacefb2c7c8160d5a8b833c625
mohdqaseem198 <qaseemshahid198@gmail.com>
2025-09-14 15:03:50 +0530

    Updates

---
+++ LazyComponent.b10d1127.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequirece0d;(0,e.register)("i70rW",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0");e("acw62");var l=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h2",{className:"font-bold text-2xl text-center m-10",children:"Hi, I'm lazy loading Components"})})});
+//# sourceMappingURL=LazyComponent.b10d1127.js.map
